7c20dd90dee60a78726f81496bb9e50a92a0a943
Alex Shchelokov <alex171174@mail.ru>
2024-03-09 19:20:30 +0300

    new settings fot actions added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,6 +10,7 @@ public class StatisticsServiceTest {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
@@ -22,6 +23,7 @@ public class StatisticsServiceTest {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 14, 5, 3, 8, 6, 11, 11, 12};
+
         long expected = 14;
 
         long actual = service.findMax(incomesInBillions);
